32aa3a95afa1ed8e98684dee6527915cb984ad44
BuilesYeison <y.builes.uribe@gmail.com>
2021-05-04 19:54:11 -0500

    Design in process, lets to create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http"
 
 import { AppComponent } from './app.component';
+import { MainInfoComponent } from './main-info/main-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
