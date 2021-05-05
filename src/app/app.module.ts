b6bdc9f06f7628affe03322edaf856b5d22e9a12
BuilesYeison <y.builes.uribe@gmail.com>
2021-05-04 20:19:36 -0500

    Designed Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http"
 
 import { AppComponent } from './app.component';
-import { MainInfoComponent } from './main-info/main-info.component';
+import {MainInfoComponent} from "./main-info/main-info.component";
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { DetailsComponent } from './details/details.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainInfoComponent
+    MainInfoComponent,
+    SearchboxComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
