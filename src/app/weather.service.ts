3ccda5b6e1341146b37777149997478736644ffd
BuilesYeison <y.builes.uribe@gmail.com>
2021-05-05 15:36:47 -0500

    http to https

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,11 +9,11 @@ export class WeatherService {
   uri:string = ""
   constructor(private http:HttpClient) { }//Create an HttpCLient instance
   getFromUserSelection(city:string){ //The user will bring us their city preferred
-    this.uri = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`;
     return this.http.get(this.uri);
   }
   getFromActualPosition(lon:string,lat:string){//obtenemos informacion del clima segun latitud y long
-    this.uri = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+    this.uri = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
     return this.http.get(this.uri);
   }
   getLocation():Promise<any>{
